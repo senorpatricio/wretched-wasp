b8cc513e86e20ee95856f231828cbaf23eb68c3b
Patrick Harding <harding1983@gmail.com>
2024-01-19 19:20:02 -0700

    added base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://senorpatricio.github.io',
-	integrations: [mdx(), sitemap()],
+	integrations: ['/wretched-wasp'],
 });
